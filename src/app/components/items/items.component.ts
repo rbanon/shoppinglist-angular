dec911d73d7af3e5e6349fff482fcc5e870da229
rbanon <banonrafael@gmail.com>
2022-08-08 21:02:54 +0200

    v1.0

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../../models/item';
+import { ItemService } from 'src/app/services/item.service';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items:Item[] = [];
+  total:number = 0;
+
+  constructor(private itemService:ItemService) { }
+
+  ngOnInit(): void {
+    // this.items = []
+    this.itemService.getItems().subscribe(data => {
+      this.items = data;
+      this.getTotal();
+    });
+  }
+
+  deleteItem(item:Item){
+    this.items = this.items.filter(x => x.id != item.id );
+    this.itemService.deleteItem(item).subscribe();
+    this.getTotal();
+    
+  }
+
+  toggleItem(item:Item){
+    this.itemService.toggleItem(item).subscribe();
+    this.getTotal();
+  }
+
+  getTotal(){
+    this.total = this.items
+                  .filter(item => !item.completed)
+                  .map (item => item.quantity * item.price)
+                  .reduce((acc,item) => acc += item, 0);
+
+
+  }
+}
